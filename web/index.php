ad85e7d8ff7bd3651b44d382ef0452ea7d31776d
Anoop Santhanam <anoop@playerslounge.co>
2019-08-22 08:38:03 +0530

    Added product type controller to main controller

--- web/index.php
+++ web/index.php
@@ -1,10 +1,11 @@
 <?php
 
 ini_set('display_errors', 1);
-require_once __DIR__.'/../vendor/autoload.php';
-$app = require __DIR__.'/../src/app.php';
-require __DIR__.'/../config/prod.php';
-require __DIR__.'/../src/controllers.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+$app = require __DIR__ . '/../src/app.php';
+require __DIR__ . '/../config/prod.php';
+require __DIR__ . '/../src/controllers.php';
+require __DIR__ . '/../src/ProductTypeController.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
