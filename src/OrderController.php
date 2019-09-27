a2f6db81b0148b2fb3eb32edc0b624f1db0306ee
Anoop Santhanam <anoop@playerslounge.co>
2019-09-27 09:57:09 +0530

    Fixed bug

--- src/OrderController.php
+++ src/OrderController.php
@@ -11,8 +11,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-require "../models/Status.php";
-require "../models/ProductType.php";
 require "../models/Product.php";
 require "../models/Order.php";
 require "../models/OrderItem.php";
